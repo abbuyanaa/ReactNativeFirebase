b9f1760eacbc87a6b8e6d2bd442eed9017161330
abbuyanaa <abbuyanaa@gmail.com>
2021-04-09 01:47:26 +0800

    Added NavigationContainer

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import UsersListScreen from "./screens/UsersListScreen";
+import UserCreateScreen from "./screens/UserCreateScreen";
+import UserDetailScreen from "./screens/UserDetailScreen";
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="UsersList" component={UsersListScreen} />
+        <Stack.Screen name="UserCreate" component={UserCreateScreen} />
+        <Stack.Screen name="UserDetail" component={UserDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/UserCreateScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const UserCreate = () => {
+  return (
+    <View>
+      <Text>User Create</Text>
+    </View>
+  );
+};
+
+export default UserCreate;
+
+const styles = StyleSheet.create({});
